f58d3e35241be6eff66f2586a91cd4a388451b17
Mahdia-123 <160815889+Mahdia-123@users.noreply.github.com>
2025-07-13 11:26:46 +0430

    added footer to my project

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
       <header className="App-header">
         <h1>Weather Search Engine</h1>
         <Weatherapp />
+        <footer>
+          This page was coded by <a href="/">Mahdia Khamoosh</a> and is{" "}
+          <a href="/">open-sourced o Github</a> hosted on{" "}
+          <a href="/">Netlify</a>
+        </footer>
       </header>
     </div>
   );
